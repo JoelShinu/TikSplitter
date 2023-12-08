34c6a9ea752c4db2da697f322e7ae60e391ae955
JoelShinu <joelshinu@yahoo.com>
2023-12-08 23:18:10 +0000

    downloader.py function to download yt videos into videos folder

--- download/downloader.py
+++ download/downloader.py
@@ -1,1 +1,18 @@
-# Python script for downloading videos.
+import os
+from pytube import YouTube
+
+
+def download_youtube(url, output_path='videos'):
+    try:
+        output_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', output_path)
+        os.makedirs(output_directory, exist_ok=True)
+
+        youtube = YouTube(url)
+        video_stream = youtube.streams.get_highest_resolution()
+
+        print(f"Downloading: {youtube.title}")
+        video_stream.download(output_directory)
+        print("Download Complete!")
+
+    except Exception as e:
+        print(f"An Error has occurred: {e}")
