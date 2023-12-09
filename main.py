65abbbdfe0e7e6814b306fa0a2c63ef7220de1b0
Jared Schoel <jared.phantom@gmail.com>
2023-12-09 23:45:11 +0000

    add account enum

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from caption import auto_caption
 from download.downloader import YouTubeDownloader
+from post.account import Account
 from post.tiktok_poster import Poster
 from utils import get_env_details
 from videos.tiktok_video import TikTokVideo
@@ -16,7 +17,7 @@ def main():
     youtube_downloader.download_video(youtube_video_url)
 
     video = TikTokVideo("videos/Vikkstar Was Having None Of It.mp4", "test")
-    poster = Poster(get_env_details("CLIP_CHIMP_SESSION_ID"))
+    poster = Poster(get_env_details(f"{Account.CLIP_CHIMP}_SESSION_ID"))
     poster.upload(video)
 
 
---
+++ post/account.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class Account(Enum):
+    CLIP_CHIMP = "CLIP_CHIMP"
