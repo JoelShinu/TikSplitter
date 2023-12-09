900468c4d42d6e4764e21c85a6381f9d0d8b0cd3
JoelShinu <joelshinu@yahoo.com>
2023-12-09 02:50:04 +0000

    abstract class

--- download/downloader.py
+++ download/downloader.py
@@ -1,11 +1,18 @@
 import os
 from pytube import YouTube
+from abc import ABC, abstractmethod
 
 
-class YouTubeDownloader:
+class Downloader(ABC):
     def __init__(self, output_path='videos'):
         self.output_path = output_path
 
+    @abstractmethod
+    def download_video(self, url):
+        pass
+
+
+class YouTubeDownloader(Downloader):
     def download_video(self, url):
         try:
             output_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', self.output_path)
@@ -14,15 +21,15 @@ class YouTubeDownloader:
             youtube = YouTube(url)
             video_stream = youtube.streams.get_highest_resolution()
 
-            print(f"Downloading: {youtube.title}")
+            print(f"Downloading YouTube video: {youtube.title}")
             video_stream.download(output_directory)
             print("Download Complete!")
 
         except Exception as e:
-            print(f"An Error has occurred: {e}")
+            print(f"An error occurred while downloading YouTube video: {e}")
 
 
 if __name__ == "__main__":
-    downloader = YouTubeDownloader()
-    video_url = input("Enter the YouTube video URL: ")
-    downloader.download_video(video_url)
+    youtube_downloader = YouTubeDownloader()
+    youtube_video_url = input("Enter the YouTube video URL: ")
+    youtube_downloader.download_video(youtube_video_url)
