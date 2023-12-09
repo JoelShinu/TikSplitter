7275b5e1de2063d9cf051984046b97be1fea1d7e
JoelShinu <joelshinu@yahoo.com>
2023-12-09 23:39:23 +0000

    updated main.py

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ def main():
 
     youtube_downloader = YouTubeDownloader(video_folder)
 
-    youtube_video_url = "https://www.youtube.com/watch?v=AjnDIbKOO8w"
+    youtube_video_url = "https://www.youtube.com/watch?v=Nq66eM-VuIE"
 
     youtube_downloader.download_video(youtube_video_url)
 
 
 def main2():
-    video_folder = ROOT_DIR / "videos"
-    video_file = 'videos/Vikkstar Was Having None Of It.mp4.mp4'
+    VIDEO1_PATH = str(ROOT_DIR / "videos/Vikkstar_Was_Having_None_Of_It.mp4")
+    VIDEO2_PATH = str(ROOT_DIR / "videos/CHUNKZ_Reveals_The_TRUTH_About_His_WEIGHT_LOSS_Journey!.mp4")
+    OUTPUT_PATH = str(ROOT_DIR / "merged_video.mp4")
+
     merger = Merger()
-    merger.merge_videos(video_file)
+    merger.merge_videos(VIDEO1_PATH, VIDEO2_PATH, OUTPUT_PATH)
 
 
 if __name__ == "__main__":
