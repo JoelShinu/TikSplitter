a11a44a0bf3ffeff2b155e7c589e6f40922d9576
Michael Ockenden <michael.ockenden@hotmail.com>
2023-12-09 04:18:12 +0000

    Add black to github actions (#2)
    * linter

--- download/downloader.py
+++ download/downloader.py
@@ -2,9 +2,11 @@ import os
 from pytube import YouTube
 
 
-def download_youtube(url, output_path='videos'):
+def download_youtube(url, output_path="videos"):
     try:
-        output_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', output_path)
+        output_directory = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "..", output_path
+        )
         os.makedirs(output_directory, exist_ok=True)
 
         youtube = YouTube(url)
