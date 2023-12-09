9094e28bfb62914c110799ce272271e3912fb55f
JoelShinu <joelshinu@yahoo.com>
2023-12-09 23:39:29 +0000

    fix main.py

--- merge/merger.py
+++ merge/merger.py
@@ -1,71 +1,39 @@
-import glob
-import logging
-import os
-from typing import List
-
 from moviepy.editor import VideoFileClip, clips_array
 from moviepy.video.fx.all import resize
-
+from moviepy.audio.fx.all import audio_fadein
 from configs.logging_config import configure_logging
 
 
 class Merger:
-    def __init__(self, root_folder="root", output_file="merged_video.mp4"):
-        self.root_folder = os.path.abspath(root_folder)
-        self.videos_folder = os.path.join(self.root_folder, "videos")
-        self.samples_folder = os.path.join(os.path.dirname(self.root_folder), "sample")
-        self.output_file = os.path.join(self.root_folder, output_file)
-        self.logger = configure_logging(self.root_folder)
+    def __init__(self):
+        self.logger = configure_logging()
 
-    def get_next_output_file(self):
-        base_name, extension = os.path.splitext(self.output_file)
-        index = 1
-        while os.path.exists(f"{base_name}_{index}{extension}"):
-            index += 1
-        return f"{base_name}_{index}{extension}"
+    def merge_videos(self, video1_path, video2_path, output_path):
+        try:
+            # Load the videos
+            video1 = VideoFileClip(video1_path)
+            video2 = VideoFileClip(video2_path)
 
-    def merge_videos(self, video_file: str):
-        sample_files = sorted(
-            glob.glob(os.path.join(self.samples_folder, "sample_video*.mp4"))
-        )
-        output_file = self.get_next_output_file()
+            # Resize both videos to 540x960
+            resized_video1 = video1.fx(resize, width=540, height=960)
+            resized_video2 = video2.fx(resize, width=540, height=960)
 
-        video_number = int(
-            os.path.splitext(os.path.basename(video_file))[0].replace("video", "")
-        )
-        sample_file = next(
-            (
-                file
-                for file in sample_files
-                if f"sample_video{video_number}" in os.path.basename(file)
-            ),
-            None,
-        )
+            # Mute video2
+            muted_video2 = resized_video2.fx(audio_fadein, 0.01)
 
-        if sample_file is not None:
-            video_clip = VideoFileClip(video_file)
-            sample_clip = VideoFileClip(sample_file)
+            # Set the volume of the muted video2 to 0
+            muted_video2 = muted_video2.volumex(0)
 
-            # Resize sample video to match the height of the video clip
-            sample_clip_resized = resize(sample_clip, height=video_clip.h)
+            # Concatenate the videos vertically
+            final_video = clips_array([[resized_video1], [muted_video2]])
 
-            # Combine video clip and sample clip horizontally
-            clips = clips_array(
-                [[video_clip, sample_clip_resized]], bg_color=(0, 0, 0)
-            )
+            # Write the final video to the output path
+            final_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
 
-            # Resize the final video to 1080x1920 - TikTok video sizing (I think)
-            final_clip = clips.resize(height=1920)
+            self.logger.info(f"Videos successfully merged and saved at {output_path}")
 
-            # Write the final video to the output file
-            final_clip.write_videofile(
-                output_file, codec="libx264", audio_codec="aac"
-            )
+        except Exception as e:
+            self.logger.error(f"An error occurred: {str(e)}")
 
-            self.logger.info(
-                f"Merged {os.path.basename(video_file)} with {os.path.basename(sample_file)}. Saved as {os.path.basename(output_file)}"
-            )
-        else:
-            self.logger.warning(
-                f"No corresponding sample video found for {os.path.basename(video_file)}"
-            )
+
+
