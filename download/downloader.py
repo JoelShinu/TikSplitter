5aa3b6a2950ff3038752ae5b1e5dab3c4f86aac2
JoelShinu <joelshinu@yahoo.com>
2023-12-09 02:44:49 +0000

    YouTubeDownloader class

--- download/downloader.py
+++ download/downloader.py
@@ -2,17 +2,27 @@ import os
 from pytube import YouTube
 
 
-def download_youtube(url, output_path='videos'):
-    try:
-        output_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', output_path)
-        os.makedirs(output_directory, exist_ok=True)
+class YouTubeDownloader:
+    def __init__(self, output_path='videos'):
+        self.output_path = output_path
 
-        youtube = YouTube(url)
-        video_stream = youtube.streams.get_highest_resolution()
+    def download_video(self, url):
+        try:
+            output_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', self.output_path)
+            os.makedirs(output_directory, exist_ok=True)
 
-        print(f"Downloading: {youtube.title}")
-        video_stream.download(output_directory)
-        print("Download Complete!")
+            youtube = YouTube(url)
+            video_stream = youtube.streams.get_highest_resolution()
 
-    except Exception as e:
-        print(f"An Error has occurred: {e}")
+            print(f"Downloading: {youtube.title}")
+            video_stream.download(output_directory)
+            print("Download Complete!")
+
+        except Exception as e:
+            print(f"An Error has occurred: {e}")
+
+
+if __name__ == "__main__":
+    downloader = YouTubeDownloader()
+    video_url = input("Enter the YouTube video URL: ")
+    downloader.download_video(video_url)
