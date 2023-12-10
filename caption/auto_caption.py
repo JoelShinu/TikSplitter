ec12bfce11053268ff5998c29dd4008aa0134543
JoelShinu <joelshinu@yahoo.com>
2023-12-10 02:17:12 +0000

    updated AutoCaptioner logic

--- caption/auto_caption.py
+++ caption/auto_caption.py
@@ -1,31 +1,70 @@
-import logging
-import os
+from pathlib import Path
+from youtube_transcript_api import YouTubeTranscriptApi
+from configs.logging_config import configure_logging
+from urllib.parse import urlparse, parse_qs
 
-import speech_recognition as sr
 
+class AutoCaptioner:
+    def __init__(self, output_path):
+        self.output_path = Path(output_path)
+        self._logging = configure_logging()
 
-def transcribe_wav(audio_path, output_file):
-    recognizer = sr.Recognizer()
+    def get_captions(self, video_url):
+        try:
+            video_id = self.extract_video_id(video_url)
+            if video_id:
+                captions = YouTubeTranscriptApi.get_transcript(
+                    video_id, languages=["en"]
+                )
+                return captions
+            else:
+                self._logging.error("Unable to extract video ID from the YouTube URL.")
+                return None
 
-    with sr.AudioFile(audio_path) as source:
-        audio = recognizer.record(source)
+        except Exception as e:
+            self._logging.error(f"An error occurred while fetching captions: {e}")
+            return None
 
-    try:
-        text = recognizer.recognize_sphinx(audio)
-        save_path = os.path.join(os.getcwd(), output_file)
+    def extract_video_id(self, video_url):
+        try:
+            parsed_url = urlparse(video_url)
+            video_id = parse_qs(parsed_url.query).get("v", [None])[0]
 
-        with open(save_path, "w") as file:
-            file.write(text)
+            if not video_id and parsed_url.path.startswith("/v/"):
+                # Handle case where video ID is in the path (e.g., /v/{video_id})
+                video_id = parsed_url.path.split("/")[-1]
 
-        logging.info(f"Transcription saved to: {output_file}")
-        return text
-    except sr.UnknownValueError:
-        logging.warning("Speech recognition could not recognize audio")
-    except sr.RequestError as e:
-        logging.warning(
-            f"Could not request results from Google Speech Recognition service: {e}"
-        )
-    except sr.WaitTimeoutError:
-        logging.warning("Timeout Error")
+            return video_id
+        except Exception as e:
+            self._logging.error(f"An error occurred while extracting video ID: {e}")
+            return None
 
-    return None
+    def save_captions_as_srt(self, captions, output_filename):
+        try:
+            output_file_path = self.output_path / (output_filename + ".srt")
+
+            with open(output_file_path, "w", encoding="utf-8") as srt_file:
+                for i, caption in enumerate(captions, start=1):
+                    srt_file.write(f"{i}\n")
+                    srt_file.write(
+                        f"{caption['start']} --> {caption['start'] + caption['duration']}\n"
+                    )
+                    srt_file.write(f"{caption['text']}\n\n")
+
+            self._logging.warning(f"Captions saved as SRT file: {output_file_path}")
+
+        except Exception as e:
+            self._logging.error(f"An error occurred while saving captions as SRT: {e}")
+
+    def save_captions_as_txt(self, captions, output_filename):
+        try:
+            output_file_path = self.output_path / (output_filename + ".txt")
+
+            with open(output_file_path, 'w', encoding='utf-8') as txt_file:
+                for i, caption in enumerate(captions, start=1):
+                    txt_file.write(f"{i}. {caption['text']}\n")
+
+            self._logging.warning(f"Captions saved as TXT file: {output_file_path}")
+
+        except Exception as e:
+            self._logging.error(f"An error occurred while saving captions as TXT: {e}")
