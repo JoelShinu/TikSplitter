0139d2494cc1469c86f0f273857d986bd5ca4505
JoelShinu <joelshinu@yahoo.com>
2023-12-10 20:43:34 +0000

    main.py

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from pathlib import Path
 from data.videos.tiktok_video import TikTokVideo
 from tik_splitter.caption.auto_caption import AutoCaptioner
 from tik_splitter.configs.utils import get_env_details
-from tik_splitter.download.downloader import YouTubeDownloader
+from tik_splitter.download.downloader import YouTubeDownloader, SampleVideoDownloader
 from tik_splitter.merge.merger import Merger
 from tik_splitter.post.account import Account
 from tik_splitter.post.tiktok_poster import Poster
+from data.sample.sample_video_data import sample_video_dict
 
 ROOT_DIR = Path.cwd()
 
 
-def main():
+def main():  # YouTube Download and TikTok Post Tester
     video_folder = ROOT_DIR / "data/videos"
     youtube_downloader = YouTubeDownloader(video_folder)
     youtube_video_url = "https://www.youtube.com/watch?v=yYXQkQAlMfU"
@@ -23,16 +24,16 @@ def main():
     poster.upload(video)
 
 
-def main2():
-    VIDEO1_PATH = str(ROOT_DIR / "data/videos/Deji_Stitches_Up_Tobi.mp4")
-    VIDEO2_PATH = str(ROOT_DIR / "data/videos/Callux_Gets_Violated.mp4")
-    OUTPUT_PATH = str(ROOT_DIR / "data/videos/merged_video.mp4")
+def main2():  # Merge Video Tester
+    VIDEO1_PATH = ROOT_DIR / "data/videos/Deji_Stitches_Up_Tobi.mp4"
+    VIDEO2_PATH = ROOT_DIR / "data/videos/Callux_Gets_Violated.mp4"
+    OUTPUT_PATH = ROOT_DIR / "data/videos/merged_video.mp4"
 
     merger = Merger()
     merger.merge_videos(VIDEO1_PATH, VIDEO2_PATH, OUTPUT_PATH)
 
 
-def main3():
+def main3():  # AutoCaptioner Tester
     VIDEO_URL = "https://www.youtube.com/watch?v=yYXQkQAlMfU"
     VIDEO_OUTPUT_PATH = ROOT_DIR / "data/videos"
     CAPTION_OUTPUT_PATH = ROOT_DIR / "data/captions"
@@ -49,10 +50,11 @@ def main3():
         logging.warning("No captions available")
 
 
+def main4():  # Sample Video Tester
+    sample_folder = ROOT_DIR / "data/sample"
+    sample_downloader = SampleVideoDownloader(sample_folder)
+    sample_downloader.download_sample_video("sample2", sample_video_dict)
+
+
 if __name__ == "__main__":
-    main3()
-
-
-# TODO:
-https://www.reddit.com/r/moviepy/comments/12gmvxf/need_help_with_subtitles/
-IO Buffer for video and caption data
+    main()
