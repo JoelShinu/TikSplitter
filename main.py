a1df26b1d430aec365ad088f5838b743fc6be010
JoelShinu <joelshinu@yahoo.com>
2023-12-10 02:21:33 +0000

    updated main.py with extra method for caption checking

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import logging
 from pathlib import Path
 
+from caption.auto_caption import AutoCaptioner
 from download.downloader import YouTubeDownloader
 from merge.merger import Merger
 from post.account import Account
@@ -13,7 +15,7 @@ ROOT_DIR = Path.cwd()
 def main():
     video_folder = ROOT_DIR / "videos"
     youtube_downloader = YouTubeDownloader(video_folder)
-    youtube_video_url = "https://www.youtube.com/watch?v=rN2tC5PWJUs"
+    youtube_video_url = "https://www.youtube.com/watch?v=yYXQkQAlMfU"
     youtube_downloader.download_video(youtube_video_url)
 
     video = TikTokVideo("videos/Vikkstar_Was_Having_None_Of_It.mp4", "test")
@@ -22,15 +24,29 @@ def main():
 
 
 def main2():
-    VIDEO1_PATH = str(ROOT_DIR / "videos/Vikkstar_Was_Having_None_Of_It.mp4")
-    VIDEO2_PATH = str(
-        ROOT_DIR / "videos/POKE_MOMENTS_28_S4.mp4"
-    )
+    VIDEO1_PATH = str(ROOT_DIR / "videos/Deji_Stitches_Up_Tobi.mp4")
+    VIDEO2_PATH = str(ROOT_DIR / "videos/Callux_Gets_Violated.mp4")
     OUTPUT_PATH = str(ROOT_DIR / "merged_video.mp4")
 
     merger = Merger()
     merger.merge_videos(VIDEO1_PATH, VIDEO2_PATH, OUTPUT_PATH)
 
 
+def main3():
+    VIDEO_URL = "https://www.youtube.com/watch?v=yYXQkQAlMfU"
+    OUTPUT_PATH = str(ROOT_DIR / "caption")
+
+    downloader = YouTubeDownloader(OUTPUT_PATH)
+    downloader.download_video(VIDEO_URL)
+
+    auto_captioner = AutoCaptioner(OUTPUT_PATH)
+    captions = auto_captioner.get_captions(VIDEO_URL)
+
+    if captions is not None:
+        auto_captioner.save_captions_as_srt(captions, "captions_output")
+    else:
+        logging.warning("No captions availal")
+
+
 if __name__ == "__main__":
-    main2()
+    main3()
