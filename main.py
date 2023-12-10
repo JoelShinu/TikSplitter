1b2e4f0ce290d2c52fd2bd89a22fa74ff78a2f77
Jared Schoel <jared.phantom@gmail.com>
2023-12-10 00:21:03 +0000

    trivial

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 from download.downloader import YouTubeDownloader
+from merge.merger import Merger
 from post.account import Account
 from post.tiktok_poster import Poster
 from utils import get_env_details
 from videos.tiktok_video import TikTokVideo
-from merge.merger import Merger
 
 ROOT_DIR = Path.cwd()
 
@@ -13,7 +13,7 @@ ROOT_DIR = Path.cwd()
 def main():
     video_folder = ROOT_DIR / "videos"
     youtube_downloader = YouTubeDownloader(video_folder)
-    youtube_video_url = "https://www.youtube.com/watch?v=AjnDIbKOO8w"
+    youtube_video_url = "https://www.youtube.com/watch?v=rN2tC5PWJUs"
     youtube_downloader.download_video(youtube_video_url)
 
     video = TikTokVideo("videos/Vikkstar_Was_Having_None_Of_It.mp4", "test")
@@ -24,7 +24,7 @@ def main():
 def main2():
     VIDEO1_PATH = str(ROOT_DIR / "videos/Vikkstar_Was_Having_None_Of_It.mp4")
     VIDEO2_PATH = str(
-        ROOT_DIR / "videos/CHUNKZ_Reveals_The_TRUTH_About_His_WEIGHT_LOSS_Journey!.mp4"
+        ROOT_DIR / "videos/POKE_MOMENTS_28_S4.mp4"
     )
     OUTPUT_PATH = str(ROOT_DIR / "merged_video.mp4")
 
@@ -33,4 +33,4 @@ def main2():
 
 
 if __name__ == "__main__":
-    main()
+    main2()
