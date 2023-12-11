dbd98a5e0423b51f786a5ca5d31a883f2dd93523
JoelShinu <joelshinu@yahoo.com>
2023-12-11 22:37:25 +0000

    logging changed to Path

--- tik_splitter/configs/logging_config.py
+++ tik_splitter/configs/logging_config.py
@@ -1,15 +1,15 @@
 import logging
-import os
+from pathlib import Path
 
 
 def configure_logging():
     # Get the directory of the script or module calling this function
-    root_folder = os.path.abspath(os.path.dirname(__file__))
+    root_folder = Path(__file__).resolve().parent
 
-    log_folder = os.path.join(root_folder, "logs")
-    os.makedirs(log_folder, exist_ok=True)
+    log_folder = root_folder / "logs"
+    log_folder.mkdir(parents=True, exist_ok=True)
 
-    log_file_path = os.path.join(log_folder, "app.log")
+    log_file_path = log_folder / "app.log"
 
     logging.basicConfig(
         level=logging.INFO,
