6201cf5d11ee4e25eedf77896a11da474763a662
JoelShinu <joelshinu@yahoo.com>
2023-12-15 02:36:38 +0000

    clean up main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
-
+from pathlib import Path
 from config import ROOT_DIR
-from data.sample.sample_video_data import sample_video_dict
 from tik_splitter.caption.auto_caption import AutoCaptioner
 from tik_splitter.download.downloader import SampleVideoDownloader, VideoDownloader
 from tik_splitter.entities.account import Account
@@ -10,19 +9,20 @@ from tik_splitter.post.tiktok_poster import Poster
 from tik_splitter.utils.utils import get_env_details
 
 
-def main():
-    video_folder = ROOT_DIR / "data/videos"
-    youtube_downloader = VideoDownloader(video_folder)
-    youtube_video_url = "https://www.youtube.com/watch?v=yYXQkQAlMfU"
+def main():  # Download and Post Tester
+    youtube_downloader = VideoDownloader()
+    youtube_video_url = "https://www.youtube.com/watch?v=0ahOSXe1ow0"
 
     video = youtube_downloader.download_video(youtube_video_url)
     poster = Poster(get_env_details(f"{Account.CLIP_CHIMP.value}_SESSION_ID"))
     poster.upload_videos(video)
 
 
-def main2():
+def main2():  # Download, Merge and Post Tester
     downloader = VideoDownloader()
-    videos = downloader.download_and_split_video("https://www.youtube.com/watch?v=YDp3Np4suO4")
+    videos = downloader.download_and_split_video(
+        "https://www.youtube.com/watch?v=0ahOSXe1ow0", start_time="00:00", end_time="05:30", clip_size=90
+    )
 
     merger = Merger()
     merged_vid = merger.merge_videos(videos[0], videos[1])
@@ -31,27 +31,27 @@ def main2():
     poster.upload_videos(merged_vid, videos[2], videos[3])
 
 
-def main3():  # AutoCaptioner Tester
-    VIDEO_URL = "https://www.youtube.com/watch?v=yYXQkQAlMfU"
-    VIDEO_OUTPUT_PATH = ROOT_DIR / "data/videos"
-    CAPTION_OUTPUT_PATH = ROOT_DIR / "data/captions"
-
-    downloader = VideoDownloader(VIDEO_OUTPUT_PATH)
-    downloader.download_video(VIDEO_URL)
-
-    auto_captioner = AutoCaptioner(CAPTION_OUTPUT_PATH)
-    captions = auto_captioner.get_captions(VIDEO_URL)
-
-    if captions is not None:
-        auto_captioner.save_captions_as_srt(captions, "captions_output")
-    else:
-        logging.warning("No captions available")
+def main3():  # Auto-Caption Tester
+    auto_caption = AutoCaptioner()
+    auto_caption.get_and_save_captions("https://www.youtube.com/watch?v=0ahOSXe1ow0")
 
 
 def main4():  # Sample Video Tester
-    sample_folder = ROOT_DIR / "data/sample"
-    sample_downloader = SampleVideoDownloader(sample_folder)
-    sample_downloader.download_sample_video("sample2", sample_video_dict)
+    sample_file = Path("C:\\Users\\jenzy\\PycharmProjects\\TikSplitter\\data\\sample\\sample_video_data.json")
+    sample = SampleVideoDownloader()
+    sample.download_sample_video("subway_surfers1", sample_file)
+
+
+def main5():  # Full Functionality Tester - Download, Split, Merge, Post
+    downloader = VideoDownloader()
+    sample = SampleVideoDownloader()
+    merger = Merger()
+    poster = Poster(get_env_details(f"{Account.CLIP_CHIMP.value}_SESSION_ID"))
+    sample_file = Path("C:\\Users\\jenzy\\PycharmProjects\\TikSplitter\\data\\sample\\sample_video_data.json")
+    vid1 = downloader.download_and_split_video("https://www.youtube.com/watch?v=aNIQ0MUkOCs")
+    vid2 = sample.download_sample_video("gta_2", sample_file)
+    vid3 = merger.merge_videos(vid1[0], vid2)
+    poster.upload_videos(vid3)
 
 
 if __name__ == "__main__":
