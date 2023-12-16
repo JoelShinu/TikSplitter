8f4724f0b7672ab88b21bd5f747dfcd6b1501fad
Jared Schoel <jared.phantom@gmail.com>
2023-12-16 19:17:14 +0000

    improve logging

--- tik_splitter/utils/logging_config.py
+++ tik_splitter/utils/logging_config.py
@@ -6,12 +6,16 @@ from config import LOG_PATH
 
 def configure_logging(name: str = "logger") -> logging.Logger:
     log_file_path = LOG_PATH / f"{name}.log"
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        handlers=[logging.FileHandler(log_file_path), logging.StreamHandler(sys.stdout)],
-    )
+    handler1 = logging.FileHandler(log_file_path)
+    handler1.setFormatter(formatter)
+    handler2 = logging.StreamHandler(sys.stdout)
+    handler2.setFormatter(formatter)
 
-    logger = logging.getLogger(f"TikSplitter-{name}")
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+
     return logger
